1c8be71c50ae97b71125b7f71a07d8c1e0d76f2e
catherine <catherinemshenry@gmail.com>
2026-02-20 16:55:37 -0700

    add debugging prints to get_first_instance_of_target_grounded_iu()

--- retico_core/helper_funcs.py
+++ retico_core/helper_funcs.py
@@ -1,21 +1,24 @@
-def get_first_instance_of_target_grounded_iu(obj, target_iu_objs:list):
+def get_first_instance_of_target_grounded_iu(obj, target_iu_objs:list, layers_deep=0):
     target_attr = 'grounded_in'
     for target_iu_obj in target_iu_objs:
         if isinstance(obj, dict) and target_attr in obj:
             grounded_in_iu = obj[target_attr]
             if isinstance(grounded_in_iu, target_iu_obj):
+                # print(f"Found grounded in IU {type(grounded_in_iu)} at layer {layers_deep}")
                 return grounded_in_iu
         elif hasattr(obj, '__dict__') and hasattr(obj, target_attr):
             grounded_in_iu = getattr(obj, target_attr)
             if isinstance(grounded_in_iu, target_iu_obj):
+                # print(f"Found grounded in IU {type(grounded_in_iu)} at layer {layers_deep}")
                 return grounded_in_iu
 
     # check if dict
+    layers_deep+=1
     if isinstance(obj, dict) and target_attr in obj:
-        return get_first_instance_of_target_grounded_iu(obj[target_attr], target_iu_objs)
+        return get_first_instance_of_target_grounded_iu(obj[target_attr], target_iu_objs, layers_deep=layers_deep)
     # check if a python obj and if it has the attr
     elif hasattr(obj, '__dict__') and hasattr(obj, target_attr):
-        return get_first_instance_of_target_grounded_iu(getattr(obj, target_attr), target_iu_objs)
+        return get_first_instance_of_target_grounded_iu(getattr(obj, target_attr), target_iu_objs, layers_deep=layers_deep)
     # the target IU(s) were not found grounded in the obj
     else:
         raise ValueError(f"Target IU(s) {target_iu_objs} not found grounded in {obj}")
