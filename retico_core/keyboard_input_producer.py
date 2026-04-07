0ad8ea8b746cfc6aac029ef9207a0fead75c0af3
catherinehenry <catherinemshenry@gmail.com>
2026-04-07 09:15:29 -0600

    temporary (ish) make keyboard_input_producer a "did Cozmo move" producer

--- retico_core/keyboard_input_producer.py
+++ retico_core/keyboard_input_producer.py
@@ -1,14 +1,12 @@
-import threading
-# cozmo
-# import cv2
+
 import time
-from datetime import datetime
 
 import numpy as np
+# from cozmoclad.clad.externalInterface.messageEngineToGame import RobotState
 
 # retico
 import retico_core
-from retico_core.robot import IACMotorAction
+from retico_core.robot import RobotStateIU #IACMotorAction,
 
 
 class UserInputModule(retico_core.AbstractProducingModule):
@@ -24,26 +22,27 @@ class UserInputModule(retico_core.AbstractProducingModule):
     @staticmethod
     def output_iu():
         # return TextIU # probably more ideal but the IAC motor action is already set up for what I need
-        return IACMotorAction
+        # return IACMotorAction
+        return RobotStateIU
 
-    def __init__(self, **kwargs):
+    def __init__(self, robot, **kwargs):
         # for exp room:exposure=0.05, gain=0.05
         super().__init__(**kwargs)
+        self.robot = robot
 
-
-        # NOTE: was seeing intermittent issues when this was in setup -- the exposure/gain was not setting correctly and would be too bright
-        # self.configure_camera()
-
-
-    def process_update(self, _):
+    def process_update(self, update_message):
+        while True:
+            self.robot.camera.image_stream_enabled = True
             time.sleep(5)
-            inp = input("Blah")
+            starting_pose = self.robot.pose.position.x_y_z
+            while True:
+                prior_pose = self.robot.pose
+                time.sleep(5)
+                # If the robot hasn't moved _at all_ don't break out of loop but if it has moved *and* is no longer moving, then break
+                if starting_pose != self.robot.pose.position.x_y_z and self.robot.pose.position.x_y_z == prior_pose.position.x_y_z:
+                    break
+
+            # inp = input("Blah")
             # output_iu.set_motor_action(motor_action=np.array([]), flow_uuid=str(uuid.uuid4()).split("-")[0], execution_uuid=f"manual_test_{datetime.now().strftime('%m_%d_%H'}"))
-            output_iu = self.create_iu(grounded_in=None)
-            output_iu.set_motor_action(motor_action=np.array([]), flow_uuid=inp, execution_uuid=f"manual_test_{datetime.now().strftime('%m_%d_%H')}")
+            output_iu = self.create_iu(None)
             return retico_core.UpdateMessage.from_iu(output_iu, retico_core.UpdateType.ADD)
-
-    def setup(self):
-        x = threading.Thread(target=self.process_update, args="_")
-        x.start()
-
